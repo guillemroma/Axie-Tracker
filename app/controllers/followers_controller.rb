2f2c97a870f977a9b5423b6dafcff3eb6cdf8972
Guillem Roma <guillemroma.lopez@gmail.com>
2022-03-29 11:43:43 +0200

    done, seeds created as well

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,14 +1,15 @@
 class FollowersController < ApplicationController
+  skip_forgery_protection :authenticate_user!
 
   def new
-
     user_followed = User.find(params[:id])
     Follower.create(followed: user_followed, follower: current_user )
-    user = user_followed
+    @user = user_followed
+
 
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: "/users/user_card", locals: { user: user }, formats: [:html] }
+      format.text { render partial: "/users/user_card", locals: { user: @user }, formats: [:html] }
     end
 
   end
@@ -18,11 +19,11 @@ class FollowersController < ApplicationController
     user_followed = User.find(params[:id])
     favorite = Follower.where(followed_id: user_followed.id, follower_id: current_user.id).first
     favorite.destroy
-    user = user_followed
+    @user = user_followed
 
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: "/users/user_card", locals: { user: user }, formats: [:html] }
+      format.text { render partial: "/users/user_card", locals: { user: @user }, formats: [:html] }
     end
 
   end
--- app/javascript/controllers/update_followers_controller.js
+++ app/javascript/controllers/update_followers_controller.js
@@ -7,48 +7,37 @@ export default class extends Controller {
 
   updateFollowers(event) {
 
+    event.preventDefault()
+    const url = this.formTarget.action
+
     if (this.formTarget.classList.contains("add")) {
-
-      event.preventDefault()
-
-      const url = this.formTarget.action
-
       fetch(url, {
         method: "POST",
         headers: { "Accept": "text/plain",
-          "X-CSRF-Token": document.querySelector("[name='csrf-token']").content
-        }
-      })
-        .then(response => response.text())
-        .then((data) => {
-          this.cardTarget.outerHTML = data;
-          this.cardTarget.querySelector('.favorite').classList.remove("add");
-          this.cardTarget.querySelector('.form-favorites-input').value = "Unfollow"
-        })
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content
+      }
+    })
+    .then(response => response.text())
+    .then((data) => {
+      this.cardTarget.outerHTML = data;
+      this.cardTarget.querySelector('.favorite').classList.remove("add");
+      this.cardTarget.querySelector('.form-favorites-input').value = "Unfollow"
+    })
 
     } else {
-
-      //destroy
-      event.preventDefault()
-
-      const url = this.formTarget.action
-
-      fetch(url, {
-        method: "DELETE",
-        headers: {
-          "Accept": "text/plain",
-          "X-CSRF-Token": document.querySelector("[name='csrf-token']").content
-        }
-      })
-        .then(response => response.text())
-        .then((data) => {
-          this.cardTarget.outerHTML = data;
-          this.cardTarget.querySelector('.favorite').classList.add("add");
-          this.cardTarget.querySelector('.form-favorites-input').value = "Follow"
-        })
-
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "Accept": "text/plain",
+        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content
+      }
+    })
+    .then(response => response.text())
+    .then((data) => {
+      this.cardTarget.outerHTML = data;
+      this.cardTarget.querySelector('.favorite').classList.add("add");
+      this.cardTarget.querySelector('.form-favorites-input').value = "Follow"
+    })
     }
-
   }
-
 }
