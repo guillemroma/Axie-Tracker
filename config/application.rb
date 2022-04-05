7eeee6925d96fb374acbab861cecc5d6d79d3b0c
Guillem Roma <guillemroma.lopez@gmail.com>
2022-04-05 14:50:27 +0200

    precomplie fixed

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,9 @@
 require_relative "boot"
-
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.assets.initialize_on_precompile = false
 
 module AxieTracker
   class Application < Rails::Application
